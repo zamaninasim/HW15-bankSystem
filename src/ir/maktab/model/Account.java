08817edbd76f992aeeaaffd87baff2cf02072ed7
nasim <zamaninasim213@gamail.com>
2021-12-13 12:40:48 +0330

    modify class naming

--- src/ir/maktab/model/Account.java
+++ src/ir/maktab/model/Account.java
@@ -1,24 +1,8 @@
 package ir.maktab.model;
-
-import ir.maktab.enumeration.UserType;
 import lombok.Data;
 
 import javax.persistence.Entity;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 @Data
 @Entity
 public class Account {
-    private Integer id;
-    private String firstName;
-    private String lastName;
-    private String nationalCode;
-    private List<Account> accounts;
-    private UserType userType;
-    private Date openingDate;
-    private Date updateInfoDate;
-    private Map<Date,String> updates;
-
 }
--- src/ir/maktab/model/User.java
+++ src/ir/maktab/model/User.java
@@ -1,8 +1,24 @@
 package ir.maktab.model;
+
+import ir.maktab.enumeration.UserType;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 @Data
 @Entity
 public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String nationalCode;
+    private List<User> accounts;
+    private UserType userType;
+    private Date openingDate;
+    private Date updateInfoDate;
+    private Map<Date,String> updates;
+
 }
