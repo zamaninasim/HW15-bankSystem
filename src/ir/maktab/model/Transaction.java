93d2da43c4b1f1572f4d14fe02e58a27e05ee25c
nasim <zamaninasim213@gamail.com>
2021-12-13 14:40:54 +0330

    add Transaction anotations

--- src/ir/maktab/model/Transaction.java
+++ src/ir/maktab/model/Transaction.java
@@ -2,11 +2,9 @@ package ir.maktab.model;
 
 import ir.maktab.enumeration.TransactionType;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
@@ -15,8 +13,10 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
     private User user;
     private TransactionType transactionType;
     private Long amount;
+    @CreationTimestamp
     private Date date;
 }
