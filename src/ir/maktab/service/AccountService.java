1a6a15861705670b164dd53fb25ba6cd4e17131b
nasim <zamaninasim213@gamail.com>
2021-12-14 14:04:54 +0330

    add acoountdao and service

--- src/ir/maktab/service/AccountService.java
+++ src/ir/maktab/service/AccountService.java
@@ -1,4 +1,17 @@
 package ir.maktab.service;
 
+import ir.maktab.dao.AccountDao;
+import ir.maktab.model.Account;
+
 public class AccountService {
+    AccountDao accountDao = new AccountDao();
+
+    public void saveUserService(Account account) {
+        accountDao.saveAccount(account);
+    }
+
+    public Account readAccountByCartNumber(Long cartNumber) {
+        Account accountWhitThisCartNumber = accountDao.findAccountByCartNumber(cartNumber);
+        return accountWhitThisCartNumber;
+    }
 }
